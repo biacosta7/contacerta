e963c3bbed5ed5ea64f2b57ada14923ec1a1fc8d
biacosta7 <contact.beatrizcosta@gmail.com>
2025-01-17 16:38:12 -0300

    user model

--- src/contacerta/urls.py
+++ src/contacerta/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ContaCertaProject project.
+URL configuration for contacerta project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
---
+++ src/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None 
+    email = models.EmailField(unique=True)
+
+    nome = models.CharField(max_length=150, blank=False)
+    sobrenome = models.CharField(max_length=150, blank=False)
+
+    USERNAME_FIELD = 'email'  # email como o campo principal de autenticação
+    REQUIRED_FIELDS = ['nome', 'sobrenome'] 
+
+    def __str__(self):
+        return self.email
