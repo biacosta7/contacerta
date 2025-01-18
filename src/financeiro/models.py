86a47113b1ada7151541310016997a6c8ac11001
biacosta7 <contact.beatrizcosta@gmail.com>
2025-01-18 10:21:36 -0300

    model financeiro

---
+++ src/financeiro/models.py
@@ -0,0 +1,127 @@
+from django.db import models
+from locais.models import Escritorio, Obra
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class Adiantamentos(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    banco = models.CharField(max_length=100)
+    observacao = models.TextField(blank=True)
+    obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.data} | {self.nome} (R${self.valor})"
+
+class Aditivo(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    banco = models.CharField(max_length=100)
+    modalidade = models.CharField(max_length=10, choices=[
+        ('prazo', 'Prazo'),
+        ('valor', 'Valor'),
+    ])
+    observacao = models.TextField(blank=True)
+    obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.data} | {self.nome} (R${self.valor})"
+
+class BM(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    banco = models.CharField(max_length=100)
+    codigo = models.CharField(max_length=50, blank=True)
+    observacao = models.TextField(blank=True)
+    obra = models.ForeignKey(Obra, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.data} | {self.nome} (R${self.valor})"
+
+
+class Despesa(models.Model):
+    descricao = models.CharField(max_length=100)
+    forma_pag = models.CharField(max_length=20, choices=[
+        ('boleto', 'Boleto'),
+        ('cartao', 'Cartão'),
+        ('pix', 'Pix'),
+        ('especie', 'Espécie'),
+    ])
+    data = models.DateField()
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    observacao = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=[
+        ('a_pagar', 'À pagar'),
+        ('pago', 'Pago'),
+    ])
+    data_pagamento = models.DateField(blank=True, null=True)
+    modalidade = models.CharField(max_length=50, choices=[
+        ('mao_de_obra', 'Mão de Obra'),
+        ('insumos', 'Insumos'),
+        ('combustivel', 'Combustível'),
+    ])
+
+    # GFK para associar com Obra ou Escritório
+    tipo_local = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    id_local = models.PositiveIntegerField()
+    local = GenericForeignKey('tipo_local', 'id_local')
+
+    def __str__(self):
+        descricao_limite = self.descricao[:20]
+        if len(self.descricao) > 20:
+            descricao_limite += '...'
+        return f"{self.local} | {descricao_limite} ({self.modalidade} | R${self.valor})"
+
+class Cartao(models.Model):
+    nome = models.CharField(max_length=100)
+    banco = models.CharField(max_length=100)
+    final = models.CharField(max_length=4)
+    vencimento = models.DateField()
+    quant_dias = models.IntegerField()
+    melhor_dia = models.DateField() # vencimento - quant_dias
+
+    def __str__(self):
+        return f"{self.final} | {self.nome} | {self.banco}"
+
+class NotaCartao(Despesa):
+    cartao = models.ForeignKey(Cartao, on_delete=models.SET_NULL, null=True)
+    quant_parcelas = models.IntegerField()
+    valor_parcela = models.DecimalField(max_digits=10, decimal_places=2)
+
+class NotaBoleto(Despesa):
+    recipiente = models.CharField(max_length=100)
+    quant_boletos = models.IntegerField()
+    vencimento = models.DateField()
+    num_notafiscal = models.CharField(max_length=50)
+    banco = models.CharField(max_length=100)
+
+class NotaPix(Despesa):
+    banco = models.CharField(max_length=100)
+
+class NotaEspecie(Despesa):
+    pagador = models.CharField(max_length=100)
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nome} - {self.cargo}"
+    
+class MaoDeObra(Despesa):
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
+    categoria = models.CharField(
+        max_length=20,
+        choices=[
+            ('adiantamento', 'Adiantamento'),
+            ('passagem', 'Passagem'),
+            ('alimentacao', 'Alimentação'),
+            ('reembolso', 'Reembolso'),
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.funcionario} - {self.categoria}"
--- src/locais/models.py
+++ src/locais/models.py
@@ -4,7 +4,7 @@ class Obra(models.Model):
     nome = models.CharField(max_length=100)
     local = models.CharField(max_length=100)
     data_inicio = models.DateField()
-    data_final = models.DateField()
+    data_final = models.DateField(blank=True, null=True)
     valor_inicial = models.DecimalField(max_digits=10, decimal_places=2)
     valor_total = models.DecimalField(max_digits=10, decimal_places=2) # valor_inicial - aditivos
     valor_receber = models.DecimalField(max_digits=10, decimal_places=2) # valor_total - (sum(adiantamentos) + sum(BMs))
@@ -12,7 +12,10 @@ class Obra(models.Model):
     debito_geral = models.DecimalField(max_digits=10, decimal_places=2) # todas as despesas com status de 'à pagar' somadas
     custo_total = models.DecimalField(max_digits=10, decimal_places=2) # todas as despesas somadas geral (independente do status)
     prazo_inicial = models.DateField()
-    prazo_atual = models.DateField()
+    prazo_atual = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
 
 class Escritorio(models.Model):
     nome = models.CharField(max_length=100)
@@ -22,3 +25,6 @@ class Escritorio(models.Model):
     ceo = models.CharField(max_length=100)
     debito_mensal = models.DecimalField(max_digits=10, decimal_places=2) # mensal, despesas com status de 'à pagar' somadas
     debito_geral = models.DecimalField(max_digits=10, decimal_places=2) # todas as despesas com status de 'à pagar' somadas
+
+    def __str__(self):
+        return f"{self.nome} (CNPJ: {self.cnpj})"
